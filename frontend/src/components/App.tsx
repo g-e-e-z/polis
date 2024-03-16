55fc0ce475e8ed1cbe68ce8255f2e7de873edbec
Alex Gordon <gordon112524@gmail.com>
2024-03-16 15:07:47 -0600

    add message component and styling

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,20 +1,9 @@
-import reactLogo from '../assets/react.svg'
-import viteLogo from '/vite.svg'
+import { TextBasedChannel } from "./channel/text-based-channel"
 
-function App() {
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-    </>
-  )
+export const App = () => {
+    return (
+        <div>
+            <TextBasedChannel/>
+        </div>
+    )
 }
-
-export default App
