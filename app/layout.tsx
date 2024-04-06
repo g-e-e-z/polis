6e9eaf2d8df36100e662bae3af2b6d8f15e383bf
Alex Gordon <gordon112524@gmail.com>
2024-04-06 14:52:59 -0600

    complete server sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ModalProvider } from "@/components/providers/modal-provider";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { cn } from "@/lib/utils";
 
@@ -30,6 +31,7 @@ export default function RootLayout({
             enableSystem={false}
             storageKey="polis-theme"
           >
+           <ModalProvider />
             {children}
           </ThemeProvider>
         </body>
