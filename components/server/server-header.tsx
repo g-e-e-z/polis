31f92e59c310c465358405c050928dc87cb18b8e
Alex Gordon <gordon112524@gmail.com>
2024-04-21 00:51:31 -0600

    Create server modal and endpoint

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -57,7 +57,10 @@ export const ServerHeader = ({
           </DropdownMenuItem>
         )}
         {isModerator && (
-          <DropdownMenuItem className="px-3 py-2 text-small cursor-pointer">
+          <DropdownMenuItem
+            onClick={() => onOpen("createChannel", { server })}
+            className="px-3 py-2 text-small cursor-pointer"
+          >
             Create Channel
             <PlusCircle className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
