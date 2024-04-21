a6eb9d87b815e9a39da75042afec4438d55f72f1
Alex Gordon <gordon112524@gmail.com>
2024-04-21 01:33:52 -0600

    delete server modal

--- app/api/servers/[serverId]/route.ts
+++ app/api/servers/[serverId]/route.ts
@@ -1,41 +1,61 @@
-import { v4 as uuidv4 } from "uuid";
 import { NextResponse } from "next/server";
 
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
-import { MemberRole } from "@prisma/client";
 
-export async function POST(req: Request) {
+export async function DELETE(
+  req: Request,
+  { params }: { params: { serverId: string }}
+
+) {
   try {
-    const { name, imageUrl } = await req.json();
     const profile = await currentProfile();
 
     if (!profile) {
       return new NextResponse("Unauthorized", { status: 401 });
     }
 
-    const server = await db.server.create({
-      data: {
-        profileId: profile.id,
-        name,
-        imageUrl,
-        inviteCode: uuidv4(),
-        channels: {
-          create: [
-            { name: "general", profileId: profile.id }
-          ]
-        },
-        members: {
-          create: [
-            { profileId: profile.id, role: MemberRole.ADMIN }
-          ]
-        }
-      }
-    });
+    const server = await db.server.delete({
+      where: {
+        id: params.serverId,
+        profileId: profile.id
+      },
+    })
 
     return NextResponse.json(server);
   } catch (error) {
-    console.log("SERVERS_POST]", error);
+    console.log("SERVERS_ID_DELETE]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { serverId: string }}
+
+) {
+  try {
+    const profile = await currentProfile();
+    const { name, imageUrl } = await req.json();
+
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const server = await db.server.update({
+      where: {
+        id: params.serverId,
+        profileId: profile.id
+      },
+      data: {
+        name,
+        imageUrl
+      }
+    })
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("SERVERS_ID_PATCH]", error);
     return new NextResponse("Internal Error", { status: 500 });
   }
 }
--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -69,7 +69,10 @@ export const ServerHeader = ({
           <DropdownMenuSeparator />
         )}
         {isAdmin && (
-          <DropdownMenuItem className="px-3 py-2 text-small cursor-pointer text-rose-500">
+          <DropdownMenuItem
+          onClick={() => onOpen("deleteServer", { server })}
+            className="px-3 py-2 text-small cursor-pointer text-rose-500"
+          >
             Delete Server
             <Trash className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
